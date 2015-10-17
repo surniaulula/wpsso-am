e479aec4f5fca6ffff42494cd3ee170bc38b4be5
JS Morisset <jsm@surniaulula.com>
2015-10-17 03:05:39 -0400

    v1.3.8

--- lib/config-gettext-extract.php
+++ lib/config-gettext-extract.php
@@ -1,5 +1,5 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
-// an extract of translatable text strings from the static config array
+// extract of translatable text strings from static config array(s)
 _x( 'Mobile App Meta', 'lib file description', 'wpsso-am' );
 _x( 'WPSSO extension to provide Apple Store / iTunes and Google Play App meta tags for Apple\'s mobile Safari and Twitter\'s App Card.', 'plugin description', 'wpsso-am' );
 ?>
