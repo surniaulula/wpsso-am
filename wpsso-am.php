ce1eb52fb9d455cd2710dc3c5737a8c04b2d9059
JS Morisset <jsm@surniaulula.com>
2017-01-07 12:11:17 -0800

    v1.7.13-1

--- wpsso-am.php
+++ wpsso-am.php
@@ -13,7 +13,7 @@
  * Description: WPSSO extension to provide Apple Store / iTunes and Google Play App meta tags for Apple's mobile Safari and Twitter's App Card.
  * Requires At Least: 3.7
  * Tested Up To: 4.7
- * Version: 1.7.13-rc1
+ * Version: 1.7.13-1
  * 
  * Version Numbering Scheme: {major}.{minor}.{bugfix}-{stage}{level}
  *
