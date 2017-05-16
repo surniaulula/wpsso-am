e1feabd8a1555dc214cdfb4bb7569f74b9b335d4
JS Morisset <jsm@surniaulula.com>
2017-05-17 01:20:58 +0200

    v1.7.19

--- wpsso-am.php
+++ wpsso-am.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO extension to provide Apple Store / iTunes and Google Play App meta tags for Apple's mobile Safari and Twitter's App Card.
  * Requires At Least: 3.7
- * Tested Up To: 4.7.4
+ * Tested Up To: 4.7.5
  * Version: 1.7.19
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
