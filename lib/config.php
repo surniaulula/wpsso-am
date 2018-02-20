11b72812198dc60e0b2034be286bfeed42c0389c
JS Morisset <jsm@surniaulula.com>
2018-02-20 07:43:27 -0600

    v1.8.0-dev.3

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoAmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoam' => array(			// Plugin acronym.
-					'version' => '1.8.0-dev.2',		// Plugin version.
+					'version' => '1.8.0-dev.3',		// Plugin version.
 					'opt_version' => '7',		// Increment when changing default option values.
 					'short' => 'WPSSO AM',		// Short plugin name.
 					'name' => 'WPSSO Mobile App Meta',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoAmConfig' ) ) {
 					'req' => array(
 						'short' => 'WPSSO Core',
 						'name' => 'WPSSO Core',
-						'min_version' => '3.53.1-dev.2',
+						'min_version' => '3.54.0-dev.3',
 					),
 					'img' => array(
 						'icons' => array(
