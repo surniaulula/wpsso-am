80f1779ad0855a0c6107ca8703a05406761275d1
JS Morisset <jsm@surniaulula.com>
2018-07-05 17:01:43 -0600

    v1.8.2

--- lib/register.php
+++ lib/register.php
@@ -16,7 +16,8 @@ if ( ! class_exists( 'WpssoAmRegister' ) ) {
 		public function __construct() {
 
 			register_activation_hook( WPSSOAM_FILEPATH, array( $this, 'network_activate' ) );
-			//register_deactivation_hook( WPSSOAM_FILEPATH, array( $this, 'network_deactivate' ) );	// Deactivation method not required.
+
+			//register_deactivation_hook( WPSSOAM_FILEPATH, array( $this, 'network_deactivate' ) );
 
 			if ( is_multisite() ) {
 				add_action( 'wpmu_new_blog', array( $this, 'wpmu_new_blog' ), 10, 6 );
