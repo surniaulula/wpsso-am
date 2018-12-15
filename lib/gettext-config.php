7c8f282bfa210986ba9e1a695c3e81a2dd2aee4f
JS Morisset <jsm@surniaulula.com>
2018-12-15 17:40:20 -0600

    v1.9.2

--- lib/gettext-config.php
+++ lib/gettext-config.php
@@ -1,4 +1,7 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
-// extract of translatable text strings from static config array
+
+/**
+ * Extract of translatable text strings from the static config array.
+ */
 _x( 'Mobile App Meta', 'lib file description', 'wpsso-am' );
 _x( 'WPSSO Core add-on to provide Apple Store / iTunes and Google Play App meta tags for Apple\'s mobile Safari and Twitter\'s App Card.', 'plugin description', 'wpsso-am' );
---
+++ lib/gettext-pro.php
@@ -0,0 +1,26 @@
+<?php die( 'These aren\'t the droids you\'re looking for...' );
+
+/**
+ * Extract of translatable text strings from the lib/pro classes.
+ */
+__( 'Save a draft version or publish the %s to display these options.', 'wpsso-am' );
+_x( 'Twitter App Card', 'metabox title', 'wpsso-am' );
+_x( 'App Store Territory', 'option label', 'wpsso-am' );
+_x( 'iPhone App Details', 'metabox title', 'wpsso-am' );
+_x( 'iPhone App ID', 'option label', 'wpsso-am' );
+_x( 'iPhone App Name', 'option label', 'wpsso-am' );
+_x( 'iPhone App URL Scheme', 'option label', 'wpsso-am' );
+_x( 'iPad App Details', 'metabox title', 'wpsso-am' );
+_x( 'iPad App ID', 'option label', 'wpsso-am' );
+_x( 'iPad App Name', 'option label', 'wpsso-am' );
+_x( 'iPad App URL Scheme', 'option label', 'wpsso-am' );
+_x( 'Google Play App Details', 'metabox title', 'wpsso-am' );
+_x( 'Google Play App ID', 'option label', 'wpsso-am' );
+_x( 'Google Play App Name', 'option label', 'wpsso-am' );
+_x( 'Google Play App URL Scheme', 'option label', 'wpsso-am' );
+_x( 'Mobile App Banner', 'metabox title', 'wpsso-am' );
+_x( 'App ID Number', 'option label', 'wpsso-am' );
+_x( 'Affiliate Data', 'option label', 'wpsso-am' );
+_x( 'Argument String', 'option label', 'wpsso-am' );
+_x( 'Show Tab on Post Types', 'option label', 'wpsso-am' );
+_x( 'Default App Store Territory', 'option label', 'wpsso-am' );
