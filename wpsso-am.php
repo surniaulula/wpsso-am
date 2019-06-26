36d7c749572cd4cb6f1129029a22091e50fe1f24
JS Morisset <jsm@surniaulula.com>
2019-06-26 06:50:24 -0700

    v2.0.0

--- wpsso-am.php
+++ wpsso-am.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO Core add-on provides Apple Store / iTunes and Google Play App meta tags for Apple's mobile Safari banner and Twitter's App Card.
  * Requires At Least: 3.8
- * Tested Up To: 5.2.1
+ * Tested Up To: 5.2.2
  * Version: 2.0.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
