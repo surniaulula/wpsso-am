faa0ace03018ea5d84636f095a031732fa32ce25
JS Morisset <jsm@surniaulula.com>
2019-08-02 13:52:34 -0700

    v2.1.0

--- lib/gettext-config.php
+++ lib/gettext-config.php
@@ -4,4 +4,4 @@
  * Extract of translatable text strings from the static config array.
  */
 _x( 'Mobile App Meta', 'lib file description', 'wpsso-am' );
-_x( 'WPSSO Core add-on provides Apple Store / iTunes and Google Play App meta tags for Apple\'s mobile Safari banner and Twitter\'s App Card.', 'plugin description', 'wpsso-am' );
+_x( 'Apple Store / iTunes and Google Play App meta tags for Apple\'s mobile Safari banner and Twitter\'s App Card.', 'plugin description', 'wpsso-am' );
