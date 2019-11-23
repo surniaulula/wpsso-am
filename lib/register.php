e00d21e425cde015e291ea12be934c5f2f9df576
JS Morisset <jsm@surniaulula.com>
2019-11-23 12:36:19 -0800

    v3.0.1

--- lib/register.php
+++ lib/register.php
@@ -102,7 +102,7 @@ if ( ! class_exists( 'WpssoAmRegister' ) ) {
 				/**
 				 * Register plugin install, activation, update times.
 				 */
-				if ( class_exists( 'WpssoUtilReg' ) ) {	// Since WPSSO v6.13.0.
+				if ( class_exists( 'WpssoUtilReg' ) ) {	// Since WPSSO v6.13.1.
 
 					$version = WpssoAmConfig::$cf[ 'plugin' ][ 'wpssoam' ][ 'version' ];
 
