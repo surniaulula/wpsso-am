0399f36f33bef3dbd5b85a1424df36f20af60afb
JS Morisset <jsm@surniaulula.com>
2020-03-26 08:08:09 -0700

    v3.2.0-b.1

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoAmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoam' => array(			// Plugin acronym.
-					'version'     => '3.2.0-dev.4',	// Plugin version.
+					'version'     => '3.2.0-b.1',	// Plugin version.
 					'opt_version' => '8',		// Increment when changing default option values.
 					'short'       => 'WPSSO AM',	// Short plugin name.
 					'name'        => 'WPSSO Mobile App Meta',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoAmConfig' ) ) {
 					'req' => array(
 						'short'       => 'WPSSO Core',
 						'name'        => 'WPSSO Core',
-						'min_version' => '6.26.0-dev.4',
+						'min_version' => '6.26.0-b.1',
 					),
 					'assets' => array(
 						'icons' => array(
