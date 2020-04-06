2d99e83616e154f89f7be1673029327b2877492f
JS Morisset <jsm@surniaulula.com>
2020-04-06 10:17:26 -0700

    v3.3.0

--- wpsso-am.php
+++ wpsso-am.php
@@ -14,7 +14,7 @@
  * Requires PHP: 5.6
  * Requires At Least: 4.2
  * Tested Up To: 5.4
- * Version: 3.3.0-rc.3
+ * Version: 3.3.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
@@ -190,6 +190,11 @@ if ( ! class_exists( 'WpssoAm' ) ) {
 			}
 
 			if ( ! $this->have_min_version ) {
+
+				if ( $this->p->debug->enabled ) {
+					$this->p->debug->log( 'exiting early: have_min_version is false' );
+				}
+
 				return;	// Stop here.
 			}
 
