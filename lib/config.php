26a4793dc26fc3007f97f7be8b4391d48584695d
JS Morisset <jsm@surniaulula.com>
2020-09-11 10:56:53 -0700

    v3.7.0

--- lib/config.php
+++ lib/config.php
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoAmConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '8.0.0',
+							'min_version'   => '8.4.0',
 						),
 					),
 
