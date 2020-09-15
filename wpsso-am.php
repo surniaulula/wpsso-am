31496994158d0f2edfbea80572a01f672b73b06b
JS Morisset <jsm@surniaulula.com>
2020-09-15 13:20:39 -0700

    v3.7.1

--- wpsso-am.php
+++ wpsso-am.php
@@ -14,7 +14,7 @@
  * Requires PHP: 5.6
  * Requires At Least: 4.2
  * Tested Up To: 5.5
- * Version: 3.7.1-rc.1
+ * Version: 3.7.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
