040b903fb8a291005d5638c79e2294307ae554c6
JS Morisset <jsm@surniaulula.com>
2020-10-15 12:50:26 -0700

    v3.8.0-b.1

--- wpsso-am.php
+++ wpsso-am.php
@@ -61,21 +61,12 @@ if ( ! class_exists( 'WpssoAm' ) ) {
 			return self::$instance;
 		}
 
-		public function init_textdomain( $debug_enabled = false ) {
+		public function init_textdomain() {
 
-			static $local_cache = null;
-
-			if ( null === $local_cache || $debug_enabled ) {
-
-				$local_cache = 'wpsso-am';
-
-				load_plugin_textdomain( 'wpsso-am', false, 'wpsso-am/languages/' );
-			}
-
-			return $local_cache;
+			load_plugin_textdomain( 'wpsso-am', false, 'wpsso-am/languages/' );
 		}
 
-		public function init_objects() {
+		public function init_objects( $is_admin, $doing_ajax, $doing_cron ) {
 
 			$this->p =& Wpsso::get_instance();
 
