a1287eaae32e4dba9bf6e28fac841d5978e370c0
JS Morisset <jsm@surniaulula.com>
2020-10-18 11:07:28 -0700

    v3.8.1

--- wpsso-am.php
+++ wpsso-am.php
@@ -66,7 +66,10 @@ if ( ! class_exists( 'WpssoAm' ) ) {
 			load_plugin_textdomain( 'wpsso-am', false, 'wpsso-am/languages/' );
 		}
 
-		public function init_objects( $is_admin ) {
+		/**
+		 * $is_admin, $doing_ajax, and $doing_cron available since WPSSO Core v8.8.0.
+		 */
+		public function init_objects( $is_admin = false, $doing_ajax = false, $doing_cron = false ) {
 
 			$this->p =& Wpsso::get_instance();
 
