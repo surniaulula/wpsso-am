ce99bb67c437611db544e641a669b45a6973eec0
JS Morisset <jsm@surniaulula.com>
2021-07-09 13:45:35 -0700

    v3.10.0

--- wpsso-am.php
+++ wpsso-am.php
@@ -13,7 +13,7 @@
  * Description: Apple Store / iTunes and Google Play App meta tags for Apple's mobile Safari banner and Twitter's App Card.
  * Requires PHP: 7.0
  * Requires At Least: 5.0
- * Tested Up To: 5.7.2
+ * Tested Up To: 5.8
  * Version: 3.10.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
