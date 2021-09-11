5e2ba96c42fc60b239431eabbec67da92b136980
JS Morisset <jsm@surniaulula.com>
2021-09-11 06:40:50 -0700

    v3.11.0-b.1

--- wpsso-am.php
+++ wpsso-am.php
@@ -14,7 +14,7 @@
  * Requires PHP: 7.0
  * Requires At Least: 5.0
  * Tested Up To: 5.8.1
- * Version: 3.11.0-dev.7
+ * Version: 3.11.0-b.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
