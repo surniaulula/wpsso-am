cf0fda6015303eab636bc344aab927da7bbd0fad
JS Morisset <jsm@surniaulula.com>
2021-11-13 20:24:56 -0800

    v3.12.2-dev.3

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -6,3 +6,4 @@
 _x( 'Mobile Apps', 'lib file description', 'wpsso-am' );
 _x( 'WPSSO Mobile App Meta Tags', 'plugin name', 'wpsso-am' );
 _x( 'Apple Store / iTunes and Google Play App meta tags for Apple\'s mobile Safari banner and Twitter\'s App Card.', 'plugin description', 'wpsso-am' );
+_x( 'WPSSO Core', 'plugin name', 'wpsso-am' );
