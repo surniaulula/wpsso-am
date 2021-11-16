aec9de84f4cd51093c32e8165e49d434cfddbb2f
JS Morisset <jsm@surniaulula.com>
2021-11-15 18:49:50 -0800

    v3.12.2

--- wpsso-am.php
+++ wpsso-am.php
@@ -11,8 +11,8 @@
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Apple Store / iTunes and Google Play App meta tags for Apple's mobile Safari banner and Twitter's App Card.
- * Requires PHP: 7.0
- * Requires At Least: 5.0
+ * Requires PHP: 7.2
+ * Requires At Least: 5.2
  * Tested Up To: 5.8.2
  * Version: 3.12.2
  * 
