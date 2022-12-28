5f7b83cc1047285e3d34abccc68d0bdb097b8153
JS Morisset <jsm@surniaulula.com>
2022-12-28 12:01:42 -0800

    v3.16.0

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -1,8 +1,7 @@
 <?php die( 'These aren\'t the droids you\'re looking for.' );
-
-/**
- * Extract of translatable strings from lib/config.php.
- */
+// 
+// Extract of translatable strings from lib/config.php.
+// 
 _x( 'Mobile Apps', 'lib file description', 'wpsso-am' );
 _x( 'WPSSO Mobile App Meta Tags', 'plugin name', 'wpsso-am' );
 _x( 'Apple Store / iTunes and Google Play App meta tags for Apple\'s mobile Safari banner and Twitter\'s App Card.', 'plugin description', 'wpsso-am' );
