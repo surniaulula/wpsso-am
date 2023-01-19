89d947e4def6744f668e38ce152bc8b908a3ad62
JS Morisset <jsm@surniaulula.com>
2023-01-19 09:45:10 -0800

    v3.17.0-rc.2

--- wpsso-am.php
+++ wpsso-am.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * Plugin Name: WPSSO Mobile App Meta Tags
  * Plugin Slug: wpsso-am
  * Text Domain: wpsso-am
@@ -15,7 +15,7 @@
  * Requires PHP: 7.2
  * Requires At Least: 5.2
  * Tested Up To: 6.1.1
- * Version: 3.17.0-rc.1
+ * Version: 3.17.0-rc.2
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
