2c494948e52bd5d8d65421f292b49910d0ad6cba
JS Morisset <jsm@surniaulula.com>
2023-01-24 09:34:24 -0800

    v3.17.1-dev.3

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoAmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoam' => array(			// Plugin acronym.
-					'version'     => '3.17.1-dev.2',	// Plugin version.
+					'version'     => '3.17.1-dev.3',	// Plugin version.
 					'opt_version' => '10',		// Increment when changing default option values.
 					'short'       => 'WPSSO AM',	// Short plugin name.
 					'name'        => 'WPSSO Mobile App Meta Tags',
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoAmConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '14.6.1-dev.2',
+							'min_version'   => '14.6.1-dev.3',
 						),
 					),
 
